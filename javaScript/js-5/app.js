75e2fa9e87457f91b60c508e965b3af6447ec8c7
Kishan Kumar Mandal <138436806+Kkumarmandal@users.noreply.github.com>
2024-08-02 16:59:27 +0530

    Add files via upload

---
+++ javaScript/js-5/app.js
@@ -0,0 +1,23 @@
+const car = {
+    name: "Mahindra",
+    model: "XUV700",
+    color: "Blue"
+};
+
+console.log (car.name);
+
+const person = {
+    name: "Kishan",
+    age: 26,
+    city: "Mumbai"
+};
+
+console.log (person);
+person.city = "New York";
+console.log(person);
+person.country = "United States";
+console.log (person);
+
+
+const diceroll = Math.floor (Math.random() * 6) + 1;
+console.log (diceroll);
